6b8fe20fc060ca74818d674887d34bf8ee9207d4
zzallang <wmfsp9@naver.com>
2022-08-22 20:26:14 +0900

    app.040-2

--- app-server/src/app/Servlet/Servlet.java
+++ app-server/src/app/Servlet/Servlet.java
@@ -4,5 +4,8 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 
 public interface Servlet {
+  String SUCESSS = "success";
+  String FAIL = "fail";
+
   void service(DataInputStream in, DataOutputStream out);
 }
--- app-server/src/app/board/servlet/BoardServlet.java
+++ app-server/src/app/board/servlet/BoardServlet.java
@@ -5,23 +5,27 @@ package app.board.servlet;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import com.google.gson.Gson;
 import app.Servlet.Servlet;
 import app.board.dao.BoardDao;
+import app.board.domain.Board;
 
 public class BoardServlet implements Servlet {
 
   // 게시글 목록을 관리할 객체 준비
   private BoardDao boardDao;
+  private String filename;
 
   public BoardServlet(String dataName) {
-    //    boardDao = new BoardDao(filename);
-    //
-    //    try { 
-    //      boardDao.load();
-    //    } catch (Exception e) {
-    //      System.out.printf("%s 파일 로딩 중 오류 발생! \n", filename);
-    //      //      e.printStackTrace();
-    //    }
+    filename = dataName + ".josn";
+    boardDao = new BoardDao(filename);
+
+    try {
+      boardDao.load();
+    } catch (Exception e) {
+      System.out.printf("%s 파일 로딩 중 오류 발생! \n", filename);
+      e.printStackTrace();
+    }
   }
 
   @Override
@@ -29,24 +33,53 @@ public class BoardServlet implements Servlet {
     try {
 
       String command = in.readUTF();
+      String json = null;
+      Board board = null;
+      int no = 0;
 
       switch (command) {
         case "findAll":
-          out.writeUTF("success");
+          Board[] boards = boardDao.findAll();
+          out.writeUTF(SUCESSS);
+          out.writeUTF(new Gson().toJson(boards));
           break;
         case "findByNo":
-          out.writeUTF("success");
+          no = in.readInt();
+          board = boardDao.findByNo(no);
+          if (board != null) {
+            out.writeUTF(SUCESSS);
+            out.writeUTF(new Gson().toJson(board));
+          } else {
+            out.writeUTF(FAIL);
+          }
           break;
         case "insert":
-          out.writeUTF("success");
-          break;
-        case "delete":
-          out.writeUTF("success");
+          json = in.readUTF();
+          board = new Gson().fromJson(json, Board.class);
+          boardDao.save();
+          boardDao.insert(board);
+          out.writeUTF(SUCESSS);
           break;
         case "update":
-          out.writeUTF("success");
+          json = in.readUTF();
+          board = new Gson().fromJson(json, Board.class);
+          if (boardDao.update(board)) {
+            boardDao.save();
+            out.writeUTF(SUCESSS);
+          } else {
+            out.writeUTF(FAIL);
+          }
+        case "delete":
+          no = in.readInt();
+          if (boardDao.delete(no)) {
+            boardDao.save();
+            out.writeUTF(SUCESSS);
+          } else {
+            out.writeUTF(FAIL); 
+          }
+          break;
         default :
-          out.writeUTF("fail");
+          out.writeUTF(FAIL);
           break;
       }
     } catch (Exception e) {
