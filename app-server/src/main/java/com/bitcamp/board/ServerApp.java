defb7b641a288ce7f9e4f71098c7bc1e69e89a7e
zzallang <wmfsp9@naver.com>
2022-09-03 14:13:34 +0900

    app45 리팩토링4

--- app-server/src/main/java/com/bitcamp/board/ServerApp.java
+++ app-server/src/main/java/com/bitcamp/board/ServerApp.java
@@ -21,44 +21,6 @@ public class ServerApp {
     servletMap.put("daily", new BoardServlet("daily"));
     servletMap.put("member", new MemberServlet("member"));
 
-    // 스레드로 만드는 대신에 Thread가 실행할 수 있는 클래스로 변경한다.
-    class RequestRunnable implements Runnable {
-
-      private Socket socket;
-
-      public RequestRunnable(Socket socket) {
-        this.socket = socket;
-      }
-
-      // 별도의 실행 흐름에서 수행할 작업 정의
-      @Override
-      public void run() {
-        try (Socket socket = this.socket;
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());) {
-
-          System.out.println("클라이언트와 연결 되었음!");
-
-          String dataName = in.readUTF();
-
-          // 로컬 클래스는 바깥 메소드의 로컬 변수를 자신의 멤버인 것 처럼 사용할 수 있다.
-          // 어떻게? 내부의 컴파일러가 그것이 가능하도록 필드와 생성자에 파라미터를 자도응로 추가한다.
-          // 
-          Servlet servlet = servletMap.get(dataName);
-          if (servlet != null) {
-            servlet.service(in, out);
-          } else {
-            out.writeUTF("fail");
-          }
-
-          System.out.println("클라이언트와 연결을 끊었음!");
-
-        } catch (Exception e) {
-          System.out.println("클라이언트 요청 처리 중 오류 발생!");
-          e.printStackTrace();
-        }
-      }
-    }
     System.out.println("[게시글 데이터 관리 서버]");
 
     try (ServerSocket serverSocket = new ServerSocket(8888);) {
@@ -66,12 +28,37 @@ public class ServerApp {
       System.out.println("서버 소켓 준비 완료!");
 
       while (true) {
-        // 클라이언트가 연결되면.
-        Socket socket = serverSocket.accept();
+        new Thread(new Runnable() {
+          Socket socket = serverSocket.accept();
 
-        // 클라이언트 요청을 처리할 스레드를 만든다.
-        // main 실행 흐름에서 분리하여 별도의 실행 흐름으로 작업을 수행시킨다.
-        new Thread(new RequestRunnable(socket)).start();
+          @Override
+          public void run() {
+            try (Socket socket = this.socket;
+                DataInputStream in = new DataInputStream(socket.getInputStream());
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());) {
+
+              System.out.println("클라이언트와 연결 되었음!");
+
+              String dataName = in.readUTF();
+
+              // 로컬 클래스는 바깥 메소드의 로컬 변수를 자신의 멤버인 것 처럼 사용할 수 있다.
+              // 어떻게? 내부의 컴파일러가 그것이 가능하도록 필드와 생성자에 파라미터를 자도응로 추가한다.
+              // 
+              Servlet servlet = servletMap.get(dataName);
+              if (servlet != null) {
+                servlet.service(in, out);
+              } else {
+                out.writeUTF("fail");
+              }
+
+              System.out.println("클라이언트와 연결을 끊었음!");
+
+            } catch (Exception e) {
+              System.out.println("클라이언트 요청 처리 중 오류 발생!");
+              e.printStackTrace();
+            }
+          }
+        }).start();
       }
 
     } catch (Exception e) {
