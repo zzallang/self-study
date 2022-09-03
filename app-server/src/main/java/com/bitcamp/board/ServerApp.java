988820128f62ae15579ccd9428915eca2a7d15d4
zzallang <wmfsp9@naver.com>
2022-09-03 13:54:49 +0900

    app45 리팩토링2

--- app-server/src/main/java/com/bitcamp/board/ServerApp.java
+++ app-server/src/main/java/com/bitcamp/board/ServerApp.java
@@ -5,34 +5,71 @@ import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Hashtable;
-import java.util.Map;
 import com.bitcamp.board.servlet.BoardServlet;
 import com.bitcamp.board.servlet.MemberServlet;
 import com.bitcamp.servlet.Servlet;
 
 public class ServerApp {
+
   public static void main(String[] args) {
+    // 클라이언트 요청을 처리할 객체 준비
+    Hashtable<String,Servlet> servletMap = new Hashtable<>();
+    servletMap.put("board", new BoardServlet("board"));
+    servletMap.put("reading", new BoardServlet("reading"));
+    servletMap.put("visit", new BoardServlet("visit"));
+    servletMap.put("notice", new BoardServlet("notice"));
+    servletMap.put("daily", new BoardServlet("daily"));
+    servletMap.put("member", new MemberServlet("member"));
+
+    class RequestThread extends Thread{
+
+      private Socket socket;
+
+      public RequestThread(Socket socket) {
+        this.socket = socket;
+      }
+
+      // 별도의 실행 흐름에서 수행할 작업 정의
+      @Override
+      public void run() {
+        try (Socket socket = this.socket;
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());) {
+
+          System.out.println("클라이언트와 연결 되었음!");
+
+          String dataName = in.readUTF();
+
+          // 로컬 클래스는 바깥 메소드의 로컬 변수를 자신의 멤버인 것 처럼 사용할 수 있다.
+          // 어떻게? 내부의 컴파일러가 그것이 가능하도록 필드와 생성자에 파라미터를 자도응로 추가한다.
+          // 
+          Servlet servlet = servletMap.get(dataName);
+          if (servlet != null) {
+            servlet.service(in, out);
+          } else {
+            out.writeUTF("fail");
+          }
+
+          System.out.println("클라이언트와 연결을 끊었음!");
+
+        } catch (Exception e) {
+          System.out.println("클라이언트 요청 처리 중 오류 발생!");
+          e.printStackTrace();
+        }
+      }
+    }
     System.out.println("[게시글 데이터 관리 서버]");
 
     try (ServerSocket serverSocket = new ServerSocket(8888);) {
 
       System.out.println("서버 소켓 준비 완료!");
 
-      // 클라이언트 요청을 처리할 객체 준비
-      Hashtable<String,Servlet> servletMap = new Hashtable<>();
-      servletMap.put("board", new BoardServlet("board"));
-      servletMap.put("reading", new BoardServlet("reading"));
-      servletMap.put("visit", new BoardServlet("visit"));
-      servletMap.put("notice", new BoardServlet("notice"));
-      servletMap.put("daily", new BoardServlet("daily"));
-      servletMap.put("member", new MemberServlet("member"));
-
       while (true) {
         // 클라이언트가 연결되면.
         Socket socket = serverSocket.accept();
 
         // 클라이언트 요청을 처리할 스레드를 만든다.
-        RequestThread t = new RequestThread(socket, servletMap);
+        RequestThread t = new RequestThread(socket);
 
         //main 실행 흐름에서 분리하여 별도의 실행 흐름으로 작업을 수행시킨다.
         t.start();
@@ -45,43 +82,4 @@ public class ServerApp {
     System.out.println("서버 종료!");
   }
 
-
-  static class RequestThread extends Thread{
-
-    private Socket socket;
-    private Map<String,Servlet> servletMap;
-
-    public RequestThread(Socket socket, Map<String,Servlet> servletMap) {
-      this.socket = socket;
-      this.servletMap = servletMap;
-    }
-
-    // 별도의 실행 흐름에서 수행할 작업 정의
-    @Override
-    public void run() {
-      try (Socket socket = this.socket;
-          DataInputStream in = new DataInputStream(socket.getInputStream());
-          DataOutputStream out = new DataOutputStream(socket.getOutputStream());) {
-
-        System.out.println("클라이언트와 연결 되었음!");
-
-        String dataName = in.readUTF();
-
-        Servlet servlet = servletMap.get(dataName);
-        if (servlet != null) {
-          servlet.service(in, out);
-        } else {
-          out.writeUTF("fail");
-        }
-
-        System.out.println("클라이언트와 연결을 끊었음!");
-
-      } catch (Exception e) {
-        System.out.println("클라이언트 요청 처리 중 오류 발생!");
-        e.printStackTrace();
-      }
-
-    }
-
-  }
 }
